657ac83504596885ee2b576315ed85933b240ed2
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-05 01:34:38 +0300

    feat: done animationsSpriteSheet

--- js/script.js
+++ js/script.js
@@ -1,15 +1,64 @@
 const canvas = document.getElementById('canvas1');
-const ctx = canvas.getContext('2d'); //default width 300px height 150px
-const CANVAS_WIDTH = canvas.width = 500;
-const CANVAS_HEIGHT = canvas.height = 1000;
+const ctx = canvas.getContext('2d');
+canvas.width = 500;
+canvas.height = 700;
+const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
 
-/* function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemiesArray.forEach(enemy => {
-    enemy.update();
-    enemy.draw();
-  });
-  gameFrame++;
+class Explosion {
+  constructor(x, y){
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth * 0.7;
+    this.height = this.spriteHeight * 0.7;
+    this.x = x - this.width/2;
+    this.y = y - this.height/2;
+    this.image = new Image();
+    this.image.src = 'assets/img/boom.png';
+    this.frame = 0;
+    this.timer = 0;
+    this.sound = new Audio();
+    this.sound.src = 'assets/sounds/ice-attack-2.wav';
+  }
+
+  update() {
+    if(this.frame === 0){
+      this.sound.play();
+      this.sound.volume = 0.2;
+    } 
+    this.timer++;
+    if(this.timer % 10 === 0){
+      this.frame++;
+    }
+    
+  }
+  draw() {
+    ctx.drawImage(this.image, this.spriteWidth * this.frame, 0,this.
+      spriteWidth, this.spriteHeight, this.x, this.y, this.width,
+      this.height);
+  }
+}
+
+window.addEventListener('click', function(e){
+  createAnimation(e);
+});
+
+function createAnimation(e) {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < explosions.length; i++){
+    explosions[i].update();
+    explosions[i].draw();
+    if(explosions[i].frame > 5){
+      explosions.splice(i, 1);
+      i--;
+    }
+  }
   requestAnimationFrame(animate);
 }
-animate(); */
+animate();
